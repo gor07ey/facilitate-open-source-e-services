efd12001158cece4a0de84d280db59e701a8cddb
gor07ey <152075976+gor07ey@users.noreply.github.com>
2023-11-27 21:00:15 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,58 @@
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+
+        console.log("Random Number: " + item);
+function generateRandomData() {
+
+}
+    data.forEach(item => {
+main();
+
+    });
+    }
+    const data = [];
+}
+    const data = generateRandomData();
+function main() {    data = generate_random_data()
+        print(f"Random Number: {item}")
+import random
+if __name__ == "__main__":
+def generate_random_data():
+
+    for item in data:
+    return data
+    main()
+
+def main():
+
+
+    data = [random.randint(1, 100) for _ in range(10)]    for (let i = 0; i < 10; i++) {
+function main() {
+    });
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+
+
+    }
+    return data;
+
+main();
+    const data = [];
+    const data = generateRandomData();
+}
+}        for (int i = 0; i < 10; i++) {
+        }
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        }
+}
+    }
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+
+        int[] data = new int[10];
